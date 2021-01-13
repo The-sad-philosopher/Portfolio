205ecd40366fe103666f67c4ef9187c9f88b9660
The-sad-philosopher <shinudonney@tuta.io>
2021-01-13 23:33:16 +0530

    ✨ Add menu toggle functionality

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,10 @@
-console.debug('Hello, World!');
+// *  toggle menu  * //
+const toggle_menu = (menu_btn_id: string, nav_id: string) => {
+  const menu_btn = document.getElementById(menu_btn_id);
+  const nav = document.getElementById(nav_id);
+
+  if (menu_btn && nav)
+    menu_btn.addEventListener('click', () => nav.classList.toggle('show-menu'));
+};
+
+toggle_menu('nav-toggle', 'nav-menu');
