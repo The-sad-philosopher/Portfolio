b6cbd9c448516c2089b094babc4cad4949c797fd
The-sad-philosopher <shinudonney@tuta.io>
2021-02-01 01:33:32 +0530

    🏗  Add routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home, About, Project, SinglePost, Post } from './';
 
 const App: React.FC = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={Project} path="/project" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
